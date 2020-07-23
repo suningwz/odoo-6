f4648609169b7a5a01aa062717915eb7de8ef3d9
XMars <xtruongptit@gmail.com>
2020-07-23 16:14:19 +0700

    [IMP] manifest file

--- tools/session_timeout/__manifest__.py
+++ tools/session_timeout/__manifest__.py
@@ -4,7 +4,8 @@
     'version': '1.0',
     'category': 'Tools',
     'description': """
-        Auto logout if user don't have any activity on opened tab
+        Auto logout if user don't have any activity (after 300 secs)on opened tab.
+        We need to improve this module so user can config timeout constant instead of fixed to 300 secs
     """,
     'depends' : ['web'],
     'demo': [],
